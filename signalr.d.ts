01928cf19f2d6a8f7c5803cceae411f6c9d76fb9
Nick Rees <nick.rees@accurro.com>
2020-04-30 12:50:27 +0100

    playing with types

--- signalr.d.ts
+++ signalr.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="@types/signalr" />
 declare module 'node-signalr2' {
-  const hubConnection: JQueryStatic
+  const hubConnection: SignalR.Hub.HubCreator
   const connection: SignalR
   const SignalR: SignalR
 }
