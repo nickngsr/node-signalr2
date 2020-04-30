298df8162cad88120b99ec7cf556b4714d3518c0
Nick Rees <nick.rees@accurro.com>
2020-04-30 13:12:47 +0100

    fun

---
+++ src/signalr.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="@types/signalr" />
+declare module 'node-signalr2' {
+  const hubConnection: SignalR.Hub.HubCreator
+  const connection: SignalR
+  const SignalR: SignalR
+}
+
+
